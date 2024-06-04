ceed7e059e132e75ff9d194b9fac8b271b864edd
Jakub Torłop <kubatorlop@gmail.com>
2024-06-04 20:24:23 +0200

    Add custom datatype

--- pyrkon.c
+++ pyrkon.c
@@ -7,6 +7,8 @@
 
 #define TICKET_REQUEST_TAG 1
 #define TICKET_REPLY_TAG 2
+#define TRAINING_REQUEST_TAG 3
+#define TRAINING_REPLY_TAG 4
 
 #define RELEASED 0
 #define HELD 1
@@ -17,9 +19,21 @@ int local_clock = 0;
 int reply_count = 0;
 int* ticket_queue;
 int ticket_queue_size = 0;
-
+int num_of_trainings;
+int* trainings;
+int training_queue_size = 0;
+int* training_queue;
+int* training_indices;
 // int[] funZone = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int ticket_state = RELEASED;
+
+MPI_Datatype MPI_PAKIET_T;
+
+typedef struct {
+    int ts;
+    int src;
+    int data;
+} packet_t;
 
 int t;
 bool pyrkon_done = false;
@@ -36,9 +50,10 @@ void request_ticket(){
     ticket_state = REQUESTED;
     local_clock++;
 
+    packet_t packet = {local_clock, rank, 0};
     for (i = 0; i < size; i++){
         if (i != rank) {
-            MPI_Send(&local_clock, 1, MPI_INT, i, TICKET_REQUEST_TAG, MPI_COMM_WORLD);
+            MPI_Send(&packet, 1, MPI_PAKIET_T, i, TICKET_REQUEST_TAG, MPI_COMM_WORLD);
         }
     }
     reply_count = 0;
@@ -49,8 +64,9 @@ void release_ticket() {
     int i;
     ticket_state = RELEASED;
     printf("Proces %d zwalnia bilet na Pyrkon\n", rank);
+    packet_t send_packet = {local_clock, rank, 0};
     for (i = 0; i < ticket_queue_size; i++) {
-        MPI_Send(&local_clock, 1, MPI_INT, ticket_queue[i], TICKET_REPLY_TAG, MPI_COMM_WORLD);
+        MPI_Send(&send_packet, 1, MPI_PAKIET_T, ticket_queue[i], TICKET_REPLY_TAG, MPI_COMM_WORLD);
     }
 
     ticket_queue_size = 0;
@@ -68,9 +84,11 @@ void pyrkon() {
         process_done_with_pyrkon = true;
     } else {
         MPI_Status status;
-        MPI_Recv(&local_clock, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        int rcvRank = status.MPI_SOURCE;
-        int rcvClock = local_clock;
+        packet_t packet;
+        MPI_Recv(&packet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        int rcvRank = packet.src;
+        int rcvClock = packet.ts;
+        int data = packet.data;
         if (status.MPI_TAG == TICKET_REQUEST_TAG) {
             printf("Proces %d otrzymał request od procesu %d ze znacznikiem czasu %d i własnym znacznikiem %d, porównanie %d\n", rank, rcvRank, rcvClock, t, (ticket_state == REQUESTED && (rcvClock, rcvRank) < (t, rank)));
             if (ticket_state == HELD || (ticket_state == REQUESTED && compare_timestamps(t, rank, rcvClock, rcvRank))) {
@@ -78,7 +96,9 @@ void pyrkon() {
                 printf("Proces %d dodaje proces %d do kolejki\n", rank, status.MPI_SOURCE);
             } else {
                 printf("Proces %d odpowiada procesowi %d\n", rank, status.MPI_SOURCE);
-                MPI_Send(&local_clock, 1, MPI_INT, status.MPI_SOURCE, TICKET_REPLY_TAG, MPI_COMM_WORLD);
+                packet_t send_packet = {local_clock, rank, 0};
+                // MPI_Send(&local_clock, 1, MPI_INT, status.MPI_SOURCE, TICKET_REPLY_TAG, MPI_COMM_WORLD);
+                MPI_Send(&send_packet, 1, MPI_PAKIET_T, status.MPI_SOURCE, TICKET_REPLY_TAG, MPI_COMM_WORLD);
             }
         } else if (status.MPI_TAG == TICKET_REPLY_TAG) {
             reply_count++;
@@ -98,8 +118,20 @@ int main(int argc, char **argv)
 {
     num_tickets = atoi(argv[1]);
     printf("Liczba biletów na Pyrkon: %d\n", num_tickets);
+    printf("Liczba warsztatów: %d\n", size);
 	MPI_Init(&argc, &argv);
-    
+    const int nitems=3; /* bo packet_t ma trzy pola */
+    int       blocklengths[3] = {1,1,1};
+    MPI_Datatype typy[3] = {MPI_INT, MPI_INT, MPI_INT};
+
+    MPI_Aint     offsets[3]; 
+    offsets[0] = offsetof(packet_t, ts);
+    offsets[1] = offsetof(packet_t, src);
+    offsets[2] = offsetof(packet_t, data);
+
+    MPI_Type_create_struct(nitems, blocklengths, offsets, typy, &MPI_PAKIET_T);
+    MPI_Type_commit(&MPI_PAKIET_T);
+
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
